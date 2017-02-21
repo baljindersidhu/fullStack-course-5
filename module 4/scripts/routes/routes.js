6e358bad838c8b056580804940962211d5940af5
Baljinder Singh <baljinders51@gmail.com>
2017-02-22 02:17:58 +0530

    Module 4 --routing completed

--- module 4/scripts/routes/routes.js
+++ module 4/scripts/routes/routes.js
@@ -6,15 +6,15 @@
   RoutesConfiguration.$inject =  ['$stateProvider', '$urlRouterProvider'];
   function RoutesConfiguration($stateProvider, $urlRouterProvider) {
     // if no URL matches then redirect to home
-    $urlRouterProvider.otherwise('/')
+    $urlRouterProvider.otherwise('/home')
     // define states for ui-router
     $stateProvider
     .state('home', {
-      url : '/',
+      url : '/home',
       templateUrl : 'templates/home.template.html'
     }) // home state definition ends here
-    .state('home.categories', {
-      url : 'categories',
+    .state('categories', {
+      url : '/categories',
       templateUrl : 'templates/categories.template.html',
       controller : 'CategoriesController as vm',
       resolve : {
@@ -23,7 +23,7 @@
         }]
       }
     }) // categories state definition ends here
-    .state('home.categories.items', {
+    .state('items', {
       url : '/items/{itemName}',
       templateUrl : 'templates/items.template.html',
       controller : 'ItemsController as vm',
